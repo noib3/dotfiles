0ae69e0c7a261a665918c16f41dc54fafc22611e
n0ibe <riccardo.mazzarini98@gmail.com>
2020-09-15 18:33:41 +0200

    Autocommit message

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,2 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("toolkit.cosmeticAnimations.enabled", false);
-user_pref("browser.urlbar.oneOffSearches", false);
 user_pref("apz.allow_zooming", true);
