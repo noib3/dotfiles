35786bef9e9e8c62e70ad44f328ee61d74f22f2f
noib3 <riccardo.mazzarini@pm.me>
2021-02-27 18:58:15 +0100

    Added home.nix for vps-treed

---
+++ machines/vps-treed/README.md
@@ -0,0 +1,7 @@
+``sh
+git clone https://github.com/noib3/dotfiles
+mkdir -p ~/.config
+ln -s ./dotfiles ~/.config/nixpkgs
+ln ~/.config/nixpkgs/machines/vps-treed/home.nix ~/.config/nixpkgs.home.nix
+home-manager switch
+``
--- machines/vps-treed/home.nix
+++ machines/vps-treed/home.nix
@@ -3,15 +3,8 @@
 let
   unstable = import <nixpkgs-unstable> { config = { allowUnfree = true; }; };
 
-  machine = "mbair";
+  machine = "vps-treed";
   theme   = "onedark";
-  font    = "RobotoMono";
-
-  alacrittyConfig = {
-    settings = lib.attrsets.recursiveUpdate
-      (import ./defaults/alacritty.nix { font = font; theme = theme; })
-      (import (./machines + "/${machine}" + /alacritty.nix));
-  };
 
   fdConfig = {
     ignores =
@@ -40,72 +33,24 @@ in {
   ];
 
   home = {
-    username      = "noibe";
-    homeDirectory = "/Users/noibe";
+    username      = "nix";
+    homeDirectory = "/home/nix";
     stateVersion  = "21.03";
 
     packages = with pkgs; [
-      # auto-selfcontrol
-      bash
-      buku
-      calcurse
+      bat
       chafa
-      coreutils
       direnv
-      duti
-      entr
-      # fd
-      ffmpeg
-      findutils
-      # firefox
-      # font-jetbrains-mono-nerd-font
-      # font-roboto-mono-nerd-font
-      # font-sf-mono-nerd-font
-      gnused
+      fd
+      fish
+      git
       gotop
-      jq
       lazygit
-      # logitech-options
-      # mactex-no-gui
-      # mas
-      # mysides
       mediainfo
-      mpv
+      neofetch
       neovim-nightly
-      # nodejs
-      # nordvpn
-      openssh
-      osxfuse
-      # pdftotext
       pfetch
-      (python39.withPackages(
-        ps: with ps; [
-          autopep8
-          black
-          flake8
-          ipython
-          isort
-          jedi
-        ]
-      ))
-      # redshift
-      rsync
-      # selfcontrol
-      # skhd
-      # skim
-      # spacebar
-      # ookla-speedtest
-      # sshfs
-      # syncthing
-      # tastyworks
-      # tccutil
-      terminal-notifier
-      transmission-remote-cli
-      # vimv
       vivid
-      wget
-      xmlstarlet
-      # yabai
     ];
 
     sessionVariables = {
@@ -118,9 +63,6 @@ in {
       LC_ALL       = "en_US.UTF-8";
       LESSHISTFILE = "$HOME/.cache/less/lesshst";
       LS_COLORS    = "$(vivid generate ${theme})";
-      PRIVATEDIR   = "$HOME/Sync/private";
-      SCRSHOTDIR   = "$HOME/Sync/screenshots";
-      SCRIPTSDIR   = "$HOME/Sync/scripts";
       FZF_ONLYDIRS_COMMAND = ''
         fd --base-directory=$HOME --hidden --type=d --color=always
       '';
@@ -133,14 +75,6 @@ in {
     };
 
     overlays = [
-      (self: super: {
-        direnv   = unstable.direnv;
-        fzf      = unstable.fzf;
-        lf       = unstable.lf;
-        python39 = unstable.python39;
-        starship = unstable.starship;
-        vimv     = unstable.vimv;
-      })
       (import (builtins.fetchTarball {
         url =
           https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
@@ -150,7 +84,6 @@ in {
 
   programs.home-manager = { enable = true; };
 
-  programs.alacritty = alacrittyConfig // { enable = true; };
   programs.bat       = batConfig       // { enable = true; };
   programs.fd        = fdConfig        // { enable = true; };
   programs.fish      = fishConfig      // { enable = true; };
