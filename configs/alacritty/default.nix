b0cf9778360a9f0cc5b3a7f42fba00fd902c58db
Riccardo Mazzarini <me@noib3.dev>
2024-06-06 16:19:58 +0800

    update alacritty's keybindings

--- configs/alacritty/default.nix
+++ configs/alacritty/default.nix
@@ -34,121 +34,121 @@ in
 
     cursor.style = "Beam";
 
-    key_bindings = [
+    keyboard.bindings = [
       {
         key = "D";
         mods = "Super";
-        chars = "\\x18\\x04"; # C-x C-d
+        chars = "\\u0018\\u0004"; # C-x C-d
       }
       {
         key = "E";
         mods = "Super";
-        chars = "\\x18\\x05"; # C-x C-e
+        chars = "\\u0018\\u0005"; # C-x C-e
       }
       {
         key = "H";
         mods = "Super";
-        chars = "\\x18\\x06"; # C-x C-f
+        chars = "\\u0018\\u0006"; # C-x C-f
       }
       {
         key = "R";
         mods = "Super";
-        chars = "\\x18\\x12"; # C-x C-r
+        chars = "\\u0018\\u0012"; # C-x C-r
       }
       {
         key = "K";
         mods = "Super";
-        chars = "\\x18\\x07"; # C-x C-g
+        chars = "\\u0018\\u0007"; # C-x C-g
       }
       {
         key = "L";
         mods = "Super";
-        chars = "\\x07"; # C-g
+        chars = "\\u0007"; # C-g
       }
       {
         key = "S";
         mods = "Super";
-        chars = "\\x13"; # C-s
+        chars = "\\u0013"; # C-s
       }
       {
         key = "T";
         mods = "Super";
-        chars = "\\x14"; # C-t
+        chars = "\\u0014"; # C-t
       }
       {
         key = "W";
         mods = "Super";
-        chars = "\\x17"; # C-w
+        chars = "\\u0017"; # C-w
       }
       {
         key = "Up";
         mods = "Super";
-        chars = "\\x15"; # C-u
+        chars = "\\u0015"; # C-u
       }
       {
         key = "Down";
         mods = "Super";
-        chars = "\\x04"; # C-d
+        chars = "\\u0004"; # C-d
       }
       {
         key = "Left";
         mods = "Super";
-        chars = "\\x01"; # C-a
+        chars = "\\u0001"; # C-a
       }
       {
         key = "Right";
         mods = "Super";
-        chars = "\\x05"; # C-e
+        chars = "\\u0005"; # C-e
       }
       {
         key = "Back";
         mods = "Super";
-        chars = "\\x15"; # C-u
+        chars = "\\u0015"; # C-u
       }
       {
         key = "Key1";
         mods = "Super";
-        chars = "\\x1b\\x4f\\x50"; # F1
+        chars = "\\u001b\\u004f\\u0050"; # F1
       }
       {
         key = "Key2";
         mods = "Super";
-        chars = "\\x1b\\x4f\\x51"; # F2
+        chars = "\\u001b\\u004f\\u0051"; # F2
       }
       {
         key = "Key3";
         mods = "Super";
-        chars = "\\x1b\\x4f\\x52"; # F3
+        chars = "\\u001b\\u004f\\u0052"; # F3
       }
       {
         key = "Key4";
         mods = "Super";
-        chars = "\\x1b\\x4f\\x53"; # F4
+        chars = "\\u001b\\u004f\\u0053"; # F4
       }
       {
         key = "Key5";
         mods = "Super";
-        chars = "\\x1b\\x5b\\x31\\x35\\x7e"; # F5
+        chars = "\\u001b\\u005b\\u0031\\u0035\\u007e"; # F5
       }
       {
         key = "Key6";
         mods = "Super";
-        chars = "\\x1b\\x5b\\x31\\x37\\x7e"; # F6
+        chars = "\\u001b\\u005b\\u0031\\u0037\\u007e"; # F6
       }
       {
         key = "Key7";
         mods = "Super";
-        chars = "\\x1b\\x5b\\x31\\x38\\x7e"; # F7
+        chars = "\\u001b\\u005b\\u0031\\u0038\\u007e"; # F7
       }
       {
         key = "Key8";
         mods = "Super";
-        chars = "\\x1b\\x5b\\x31\\x39\\x7e"; # F8
+        chars = "\\u001b\\u005b\\u0031\\u0039\\u007e"; # F8
       }
       {
         key = "Key9";
         mods = "Super";
-        chars = "\\x1b\\x5b\\x32\\x30\\x7e"; # F9
+        chars = "\\u001b\\u005b\\u0032\\u0030\\u007e"; # F9
       }
     ] ++ optionals isLinux [
       {
@@ -165,42 +165,42 @@ in
       {
         key = "LBracket";
         mods = "Alt|Shift";
-        chars = "\\x7B"; # {
+        chars = "\\u007B"; # {
       }
       {
         key = "RBracket";
         mods = "Alt|Shift";
-        chars = "\\x7D"; # }
+        chars = "\\u007D"; # }
       }
       {
         key = "LBracket";
         mods = "Alt";
-        chars = "\\x5B"; # [
+        chars = "\\u005B"; # [
       }
       {
         key = "RBracket";
         mods = "Alt";
-        chars = "\\x5D"; # ]
+        chars = "\\u005D"; # ]
       }
       {
         key = 23;
         mods = "Alt";
-        chars = "\\x7E"; # ~
+        chars = "\\u007E"; # ~
       }
       {
         key = 41;
         mods = "Alt";
-        chars = "\\x40"; # @
+        chars = "\\u0040"; # @
       }
       {
         key = 39;
         mods = "Alt";
-        chars = "\\x23"; # #
+        chars = "\\u0023"; # #
       }
       {
         key = 10;
         mods = "Alt";
-        chars = "\\x60"; # `
+        chars = "\\u0060"; # `
       }
     ];
   };
