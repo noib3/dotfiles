b2eba33549c95511526afc5f9ccf3fde49111f19
Riccardo Mazzarini <me@noib3.dev>
2024-07-15 18:23:53 +0800

    add `/hosts`

---
+++ hosts/default.nix
@@ -0,0 +1,62 @@
+# System-wide config shared by every host.
+
+{ lib
+, pkgs
+, homedir ? "/home/${username}"
+, hostname ? "nixos"
+, username ? "noib3"
+}:
+
+{
+  boot = {
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+    ];
+  };
+
+  console.keyMap = "us";
+
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.neovim
+  ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  hardware.bluetooth = {
+    enable = true;
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+  ];;
+
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = true;
+  }
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  time.timeZone = "Asia/Singapore";
+
+  users.users."${username}" = {
+    home = homeDirectory;
+    shell = pkgs.fish;
+    isNormalUser = true;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+}
---
+++ hosts/skunk/configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../default.nix
+  ];
+
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      efi.efiSysMountPoint = "/boot/efi";
+      systemd-boot.enable = true;
+    };
+  };
+
+  hardware.firmware = [
+    (pkgs.stdenvNoCC.mkDerivation (final: {
+      name = "broadcom-firmware";
+      # Contains the Broadcom Bluetooth and WiFi firmware, and can be
+      # downloaded by executing this script:
+      # https://wiki.t2linux.org/tools/firmware.sh
+      src = ./firmware.tar;
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/lib/firmware/brcm
+        tar -xf ${final.src} -C $out/lib/firmware/brcm
+      '';
+    }))
+  ];
+
+  # This option defines the first version of NixOS installed on this particular
+  # machine, and should NEVER be changed after the initial install.
+  system.stateVersion = "24.11";
+}
