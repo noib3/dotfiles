c19b8a1be14e29c328856842271800483b7e43bf
Riccardo Mazzarini <me@noib3.dev>
2024-07-21 13:44:00 +0800

    neovim: remap `Tab` select next completion/snippet

--- home/neovim/lua/plugins/nvim-cmp.lua
+++ home/neovim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
+--- @param key string
+local fallback = function(key)
+  local keys = vim.api.nvim_replace_termcodes(key, true, false, true)
+  vim.api.nvim_feedkeys(keys, "n", false)
+end
+
 return {
   {
     "hrsh7th/nvim-cmp",
@@ -12,9 +18,28 @@ return {
       return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
+          { name = "path" },
         }),
       }
+    end,
+    config = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      vim.api.nvim_set_keymap("i", "<Tab>", "", {
+        desc = "Select next completion or jump to next snippet or fallback",
+        callback = function()
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback("<Tab>")
+          end
+        end,
+      })
+
+      cmp.setup(opts)
     end,
   }
 }
