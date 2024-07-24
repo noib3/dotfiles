5ff9203de2e7fb096b20f7ac394fe70da1fd4573
Riccardo Mazzarini <me@noib3.dev>
2024-07-24 12:55:26 +0800

    firefox: disable custom CSS and add extensions

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -1,9 +1,10 @@
-{ colorscheme
+{ pkgs
+, lib
+, colorscheme
 , font-family
 , machine
 , palette
 , hexlib
-, lib
 }:
 
 let
@@ -19,6 +20,11 @@ in
       #   # downloads-sidebar
       #   # tridactyl-no-new-tab
       # ];
+
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        proton-pass
+        tridactyl
+      ];
 
       settings = {
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
@@ -56,52 +62,52 @@ in
         }
       '';
 
-      userChrome = ''
-        * {
-          font-family: "${font.family}";
-          font-size: ${builtins.toString font.size}px !important;
-        }
-
-        :root {
-          --color-unfocused-tabs-bg: ${colors.tabs.unfocused.bg};
-          --color-unfocused-tabs-fg: ${colors.tabs.unfocused.fg};
-
-          --color-focused-tabs-bg: ${colors.tabs.focused.bg};
-          --color-focused-tabs-fg: ${colors.tabs.focused.fg};
-
-          --color-urlbar-bg: ${colors.urlbar.bg};
-          --color-urlbar-fg: ${colors.urlbar.fg};
-          --color-urlbar-popup-url: ${colors.urlbar.urls.fg};
-          --color-urlbar-separator: ${colors.urlbar.separator};
-
-          --color-urlbar-selected-bg: ${colors.urlbar.selected.bg};
-          --color-urlbar-selected-fg: ${colors.urlbar.selected.fg};
-          --color-urlbar-selected-popup_url: ${colors.urlbar.selected.fg};
-
-          --color-sidebar-bg: ${colors.sidebar.bg};
-          --color-sidebar-fg: ${colors.sidebar.fg};
-        }
-      ''
-      + builtins.readFile ./colors.css
-      + builtins.readFile ./tabbar/debloat-tabbar.css
-      + builtins.readFile ./tabbar/tabbar-layout.css
-      + builtins.readFile ./tabbar/tabs-fill-available-width.css
-      + builtins.readFile ./tabbar/numbered-tabs.css
-      + builtins.readFile ./tabbar/tab-close-button-always-on-hover.css
-      + builtins.readFile ./tabbar/hide-tabs-with-one-tab.css
-      + builtins.readFile ./navbar/debloat-navbar.css
-      + builtins.readFile ./navbar/navbar-layout.css
-      + builtins.readFile ./navbar/navbar-on-focus.css
-      + builtins.readFile ./urlbar/debloat-urlbar.css
-      + builtins.readFile ./urlbar/urlbar-layout.css
-      + builtins.readFile ./urlbar/remove-megabar.css
-      + builtins.readFile ./sidebar/debloat-sidebar.css
-      + builtins.readFile ./sidebar/sidebar-layout.css
-      + ''
-        #PersonalToolbar {
-          display: none !important;
-        }
-      '';
+      # userChrome = ''
+      #   * {
+      #     font-family: "${font.family}";
+      #     font-size: ${builtins.toString font.size}px !important;
+      #   }
+      #
+      #   :root {
+      #     --color-unfocused-tabs-bg: ${colors.tabs.unfocused.bg};
+      #     --color-unfocused-tabs-fg: ${colors.tabs.unfocused.fg};
+      #
+      #     --color-focused-tabs-bg: ${colors.tabs.focused.bg};
+      #     --color-focused-tabs-fg: ${colors.tabs.focused.fg};
+      #
+      #     --color-urlbar-bg: ${colors.urlbar.bg};
+      #     --color-urlbar-fg: ${colors.urlbar.fg};
+      #     --color-urlbar-popup-url: ${colors.urlbar.urls.fg};
+      #     --color-urlbar-separator: ${colors.urlbar.separator};
+      #
+      #     --color-urlbar-selected-bg: ${colors.urlbar.selected.bg};
+      #     --color-urlbar-selected-fg: ${colors.urlbar.selected.fg};
+      #     --color-urlbar-selected-popup_url: ${colors.urlbar.selected.fg};
+      #
+      #     --color-sidebar-bg: ${colors.sidebar.bg};
+      #     --color-sidebar-fg: ${colors.sidebar.fg};
+      #   }
+      # ''
+      # + builtins.readFile ./colors.css
+      # + builtins.readFile ./tabbar/debloat-tabbar.css
+      # + builtins.readFile ./tabbar/tabbar-layout.css
+      # + builtins.readFile ./tabbar/tabs-fill-available-width.css
+      # + builtins.readFile ./tabbar/numbered-tabs.css
+      # + builtins.readFile ./tabbar/tab-close-button-always-on-hover.css
+      # + builtins.readFile ./tabbar/hide-tabs-with-one-tab.css
+      # + builtins.readFile ./navbar/debloat-navbar.css
+      # + builtins.readFile ./navbar/navbar-layout.css
+      # + builtins.readFile ./navbar/navbar-on-focus.css
+      # + builtins.readFile ./urlbar/debloat-urlbar.css
+      # + builtins.readFile ./urlbar/urlbar-layout.css
+      # + builtins.readFile ./urlbar/remove-megabar.css
+      # + builtins.readFile ./sidebar/debloat-sidebar.css
+      # + builtins.readFile ./sidebar/sidebar-layout.css
+      # + ''
+      #   #PersonalToolbar {
+      #     display: none !important;
+      #   }
+      # '';
     };
   };
 }
