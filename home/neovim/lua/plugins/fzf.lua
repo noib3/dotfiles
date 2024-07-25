65239192b4b2619a658820ddebce739fb38b8b97
Riccardo Mazzarini <me@noib3.dev>
2024-07-25 10:28:19 +0800

    neovim: add `fzf-lua`

---
+++ home/neovim/lua/plugins/fzf.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      winopts = {
+        backdrop = 100,
+        border = { "", "", "", "", "─", "─", "─", "" },
+        col = 0.0,
+        row = 0.0,
+        height = 8,
+        width = 1.0,
+      },
+    },
+  }
+}
