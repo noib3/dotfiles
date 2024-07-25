67293e36bdf56f490cdfa4eb791af542d258142c
Riccardo Mazzarini <me@noib3.dev>
2024-07-25 09:42:14 +0800

    neovim: set the shiftwidth to 2 in markdown files

--- home/neovim/after/ftplugin/markdown.lua
+++ home/neovim/after/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
 vim.bo.commentstring = "<!-- %s -->"
+vim.bo.spelllang = "en_us,it"
+vim.bo.textwidth = 79
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
 vim.wo.foldcolumn = "0"
 vim.wo.foldenable = false
 vim.wo.spell = false
-vim.bo.spelllang = "en_us,it"
-vim.bo.textwidth = 79
