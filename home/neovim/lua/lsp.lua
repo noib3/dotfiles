6ede8250ab5ed3b7970dcaae6dc008463e52c5ec
Riccardo Mazzarini <me@noib3.dev>
2024-07-27 00:10:34 +0800

    neovim: check LSP capabilities when attaching

--- home/neovim/lua/lsp.lua
+++ home/neovim/lua/lsp.lua
@@ -1,43 +1,55 @@
 local keymap = vim.keymap
+local methods = vim.lsp.protocol.Methods
 
 local lsp_group = vim.api.nvim_create_augroup("noib3/format-buffer", {})
 
-local on_attach = function(_ --[[ client ]], bufnr)
+local on_attach = function(client, bufnr)
   local opts = { buffer = bufnr }
 
+  -- Format buffer on save w/ a 1s timeout.
+  if client.supports_method(methods.textDocument_formatting) then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = lsp_group,
+      buffer = bufnr,
+      desc = "Formats the buffer before saving it to disk",
+      callback = function() vim.lsp.buf.format({}, 1000) end,
+    })
+  end
+
   -- Display infos about the symbol under the cursor in a floating window.
-  keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  if client.supports_method(methods.textDocument_hover) then
+    keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  end
 
   -- Rename the symbol under the cursor.
-  local inc_rename = function()
-    return ":IncRename " .. vim.fn.expand("<cword>")
+  if client.supports_method(methods.textDocument_rename) then
+    local inc_rename = function()
+      return ":IncRename " .. vim.fn.expand("<cword>")
+    end
+    keymap.set("n", "R", inc_rename, { expr = true, buffer = bufnr })
   end
-  keymap.set("n", "R", inc_rename, { expr = true, buffer = bufnr })
 
   -- Selects a code action available at the current cursor position.
-  keymap.set("n", "gca", vim.lsp.buf.code_action, opts)
+  if client.supports_method(methods.textDocument_codeAction) then
+    keymap.set("n", "gca", vim.lsp.buf.code_action, opts)
+  end
 
   -- Jumps to the definition of the symbol under the cursor.
-  keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  if client.supports_method(methods.textDocument_definition) then
+    keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  end
 
   -- Jumps to the definition of the type of the symbol under the cursor.
-  keymap.set("n", "gtd", vim.lsp.buf.type_definition, opts)
-
-  -- Format buffer on save w/ a 1s timeout.
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = lsp_group,
-    buffer = bufnr,
-    desc = "Formats the buffer before saving it to disk",
-    callback = function() vim.lsp.buf.format({}, 1000) end,
-  })
+  if client.supports_method(methods.textDocument_typeDefinition) then
+    keymap.set("n", "gtd", vim.lsp.buf.type_definition, opts)
+  end
 end
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_group,
   desc = "Set up LSP environment on a buffer",
   callback = function(args)
-    local client
-    vim.lsp.get_client_by_id(args.data.client_id)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
     on_attach(client, args.buffer)
   end,
 })
