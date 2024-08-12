a55582714defc81b4fbc5b775ae5df3aa70437b9
Riccardo Mazzarini <me@noib3.dev>
2024-08-12 09:47:49 +0800

    ags: use typescript

--- home/ags/default.nix
+++ home/ags/default.nix
@@ -3,5 +3,15 @@
 {
   configDir = ./.;
 
+  enable = pkgs.stdenv.isLinux;
+
   extraPackages = with pkgs; [ cowsay ];
+
+  package = pkgs.hiPrio (
+    pkgs.writeShellApplication {
+      name = "ags";
+      runtimeInputs = [ pkgs.bun ];
+      text = "${pkgs.ags}";
+    }
+  );
 }
