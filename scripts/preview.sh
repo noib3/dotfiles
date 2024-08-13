96d71c86cf4275a011160a0bd74bd4e56d619787
Riccardo Mazzarini <me@noib3.dev>
2024-08-13 16:53:58 +0800

    scripts: refactor `preview`

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,32 +1,21 @@
-CACHE_DIR="$HOME/.cache/image-previews"
-mkdir -p "$CACHE_DIR"
-
 FILE="$1"
 
 if [ -n "${FZF_PREVIEW_COLUMNS-}" ]; then
-  # fzf previews
+  # fzf previews.
   read -r _ TERMINAL_COLUMNS < <(</dev/tty stty size)
   WIDTH="$FZF_PREVIEW_COLUMNS"
   HEIGHT="$FZF_PREVIEW_LINES"
   X=$((TERMINAL_COLUMNS - FZF_PREVIEW_COLUMNS - 1))
   Y=0
 else
-  # lf previews
+  # lf previews.
   WIDTH="$2"
-  WIDTH=$((WIDTH - 1)) # if drawbox option is enabled
+  # If the drawbox option is enabled.
+  WIDTH=$((WIDTH - 1))
   HEIGHT="$3"
   X="$4"
   Y="$5"
 fi
-
-function hash() {
-  hash="$(
-    stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" \
-      | sha256sum \
-      | awk '{print $1}' \
-  )"
-  printf '%s/%s' "$CACHE_DIR" "$hash"
-}
 
 function draw() {
   if [ -n "${UB_SOCKET-}" ] && [ -z "${FZF_PREVIEW_COLUMNS-}" ]; then
@@ -38,9 +27,19 @@ function draw() {
     # cleaner script won't get called.
     exit 1
   else
-    chafa --size "${WIDTH}x${HEIGHT}" "$1"
+    chafa --size "${WIDTH}x${HEIGHT}" "$FILE"
   fi
 }
+
+CACHE_DIR="$HOME/.cache/image-previews"
+mkdir -p "$CACHE_DIR"
+
+CACHE_FILE="$(
+  stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$FILE")" \
+    | sha256sum \
+    | awk '{print $1}' \
+)"
+CACHE="$CACHE_DIR/$CACHE_FILE"
 
 case "$FILE" in
   *.7z|*.a|*.ace|*.alz|*.arc|*.arj|*.bz|*.bz2|*.cab|*.cpio|*.deb|*.gz|*.jar|\
@@ -49,16 +48,12 @@ case "$FILE" in
     als -- "$FILE"
     ;;
   *.svg)
-    cache="$(hash "$FILE").png"
-    [ -f "$cache" ] \
-      || inkscape -z -w 1024 -h 1024 "$FILE" -o "$cache"
-    draw "$cache"
+    [ -f "$CACHE" ] || inkscape -z -w 1024 -h 1024 "$FILE" -o "$CACHE"
+    draw "$CACHE"
     ;;
   *.epub)
-    cache="$(hash "$FILE").jpg"
-    [ -f "$cache" ] \
-      || ebook-meta --get-cover="$cache" "$FILE" &>/dev/null
-    draw "$cache"
+    [ -f "$CACHE" ] || ebook-meta --get-cover="$CACHE" "$FILE" &>/dev/null
+    draw "$CACHE"
     ;;
 esac
 
@@ -67,24 +62,15 @@ case "$(file -Lb --mime-type -- "$FILE")" in
     bat --color=always "$FILE"
     ;;
   */pdf)
-    cache="$(hash "$FILE")"
-    [ -f "${cache}.jpg" ] \
-      || pdftoppm -f 1 -l 1 \
-          -scale-to-x '1920' \
-          -scale-to-y -1 \
-          -singlefile \
-          -jpeg \
-          -- "$FILE" "$cache"
-    draw "${cache}.jpg"
+    [ -f "${CACHE}.jpg" ] || pdftoppm -singlefile -f 1 -jpeg "$FILE" "$CACHE"
+    draw "${CACHE}.jpg"
     ;;
   image/*)
     draw "$FILE"
     ;;
   video/*)
-    cache="$(hash "$FILE").jpg"
-    [ -f "$cache" ] \
-      || ffmpegthumbnailer -i "$FILE" -o "$cache" -s 0
-    draw "$cache"
+    [ -f "$CACHE" ] || ffmpegthumbnailer -i "$FILE" -o "$CACHE" -s 0
+    draw "$CACHE"
     ;;
   audio/*)
     mediainfo "$FILE"
