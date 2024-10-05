c634999c460981e1b0c221dafad3ac1af4dd8112
Riccardo Mazzarini <me@noib3.dev>
2024-10-05 18:04:27 +0800

    ags: enable bar, add a clock

--- modules/home/ags/main.ts
+++ modules/home/ags/main.ts
@@ -1,9 +1,10 @@
-import _Bar from './widgets/bar';
+import Bar from './widgets/bar';
 import OSD from './widgets/osd';
 import { forMonitors } from './lib/utils';
 
 App.config({
   windows: [
+    ...forMonitors(Bar),
     ...forMonitors(OSD),
   ],
 })
--- modules/home/ags/widgets/bar/index.ts
+++ modules/home/ags/widgets/bar/index.ts
@@ -1,7 +1,18 @@
-const time = Variable('', {
-  poll: [1000, function() {
-    return Date().toString();
-  }],
+// const hyprland = await Service.import("hyprland")
+// const notifications = await Service.import("notifications")
+// const mpris = await Service.import("mpris")
+// const audio = await Service.import("audio")
+// const battery = await Service.import("battery")
+// const systemtray = await Service.import("systemtray")
+
+const hyprland_gaps_out = 20;
+
+const date = Variable("", {
+  poll: [500, 'date +"%a %-d %b"'],
+})
+
+const time = Variable("", {
+  poll: [500, 'date +"%-H:%M"'],
 })
 
 const Bar = (monitor: number) => Widget.Window({
@@ -9,16 +20,44 @@ const Bar = (monitor: number) => Widget.Window({
   name: `bar${monitor}`,
   anchor: ['top', 'left', 'right'],
   exclusivity: 'exclusive',
+  margins: [hyprland_gaps_out, hyprland_gaps_out, 0, hyprland_gaps_out],
   child: Widget.CenterBox({
     start_widget: Widget.Label({
-      hpack: 'center',
-      label: 'Welcome to AGS!',
+      class_name: "workspaces",
+      label: "",
     }),
-    end_widget: Widget.Label({
-      hpack: 'center',
-      label: time.bind(),
+    center_widget: Widget.Label({
+      class_name: "media",
+      label: "",
     }),
+    end_widget: Clock(),
   }),
 })
+
+function Clock() {
+  return Widget.Box({
+    hpack: "end",
+    spacing: 6,
+    "margin-right": 10,
+    children: [
+      Date(),
+      Time(),
+    ],
+  })
+}
+
+function Date() {
+  return Widget.Label({
+    class_name: "date",
+    label: date.bind(),
+  })
+}
+
+function Time() {
+  return Widget.Label({
+    class_name: "time",
+    label: time.bind(),
+  })
+}
 
 export default Bar;
