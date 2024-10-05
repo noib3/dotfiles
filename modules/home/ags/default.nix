fd39902628a21a21efe903e0f54d174266a7bd1c
Riccardo Mazzarini <me@noib3.dev>
2024-10-05 18:32:54 +0800

    ags: run it on start

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -14,20 +14,50 @@ in
     enable = mkEnableOption "Ags";
   };
 
-  config = mkIf cfg.enable ({
-    home.packages = [
-      (pkgs.hiPrio (
-        pkgs.writeShellApplication {
-          name = "ags";
-          runtimeInputs = [ pkgs.bun ];
-          text = "${pkgs.ags}/bin/ags";
-        }
-      ))
-    ];
+  config = mkIf cfg.enable (
+    let
+      package = (
+        pkgs.hiPrio (
+          pkgs.writeShellApplication {
+            name = "ags";
+            runtimeInputs = [ pkgs.bun ];
+            text = "${pkgs.ags}/bin/ags";
+          }
+        )
+      );
+    in
+    {
+      # TODO: remove after https://github.com/Aylur/ags/issues/541 is fixed.
+      home.packages = [
+        package
+      ];
 
-    programs.ags = {
-      enable = true;
-      configDir = ./.;
-    };
-  });
+      programs.ags = {
+        enable = true;
+        configDir = ./.;
+        # TODO: enable after https://github.com/Aylur/ags/issues/541 is fixed.
+        systemd.enable = false;
+      };
+
+      # TODO: remove after https://github.com/Aylur/ags/issues/541 is fixed.
+      systemd.user.services.ags = {
+        Unit = {
+          Description = "AGS";
+          Documentation = "https://github.com/Aylur/ags";
+          PartOf = [ "graphical-session.target" ];
+          After = [ "graphical-session-pre.target" ];
+        };
+
+        Service = {
+          ExecStart = "${package}/bin/ags -b hypr";
+          Restart = "on-failure";
+          KillMode = "mixed";
+        };
+
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+      };
+    }
+  );
 }
