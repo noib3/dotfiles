b1474658f5e9c6e96055c0f5dd44212f963b9232
Riccardo Mazzarini <me@noib3.dev>
2024-11-08 14:51:41 +0800

    Show media keys by default in TouchBar

--- modules/nixos/hardware-apple-macbook-pro-15-1.nix
+++ modules/nixos/hardware-apple-macbook-pro-15-1.nix
@@ -12,27 +12,38 @@ in
 {
   options.hardware.apple.macbook-pro-15-1 = {
     enable = mkEnableOption ''
-      Enables configurations for the MacBook Pro 15,1
+      Enables configurations for the MacBookPro15,1 line
     '';
   };
 
   config = mkIf cfg.enable {
     caches.apple-t2.enable = true;
 
-    hardware.firmware = [
-      (pkgs.stdenvNoCC.mkDerivation (final: {
-        name = "broadcom-firmware";
-        # Contains the Broadcom Bluetooth and WiFi firmware, and can be
-        # downloaded by executing this script:
-        # https://wiki.t2linux.org/tools/firmware.sh
-        src = ./firmware.tar;
-        dontUnpack = true;
-        installPhase = ''
-          mkdir -p $out/lib/firmware/brcm
-          tar -xf ${final.src} -C $out/lib/firmware/brcm
-        '';
-      }))
-    ];
+    hardware = {
+      apple-t2.enableTinyDfr = false;
+      apple.touchBar = {
+        enable = true;
+        settings = {
+          # Show the media keys by default, and F{1-12} when the Fn key is
+          # held.
+          MediaLayerDefault = true;
+        };
+      };
+      firmware = [
+        (pkgs.stdenvNoCC.mkDerivation (final: {
+          name = "broadcom-firmware";
+          # Contains the Broadcom Bluetooth and WiFi firmware, and can be
+          # downloaded by executing this script:
+          # https://wiki.t2linux.org/tools/firmware.sh
+          src = ./firmware.tar;
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/lib/firmware/brcm
+            tar -xf ${final.src} -C $out/lib/firmware/brcm
+          '';
+        }))
+      ];
+    };
 
     # Leaving these set to "suspend" (the default) causes the system to remain
     # in an unresponsive state when the lid is re-opened.
