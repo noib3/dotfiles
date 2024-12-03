a3a5b2062a432596cbcd209e9fd716b90f3b47ea
Riccardo Mazzarini <me@noib3.dev>
2024-12-03 18:21:35 +0800

    Fix DuckDuckGo's search pattern

--- configs/qutebrowser/default.nix
+++ configs/qutebrowser/default.nix
@@ -27,7 +27,7 @@ in
   );
 
   searchEngines = {
-    "DEFAULT" = "https://duckduckgo.com/{}";
+    "DEFAULT" = "https://duckduckgo.com/?q={}";
     "yt" = "https://youtube.com/results?search_query={}";
     "nixo" = "https://search.nixos.org/options?channel=unstable&query={}";
     "nixp" = "https://search.nixos.org/packages?channel=unstable&query={}";
