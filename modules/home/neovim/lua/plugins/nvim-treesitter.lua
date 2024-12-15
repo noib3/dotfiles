161f6e0c61864ec9748effa0bb94014e925ca284
Riccardo Mazzarini <me@noib3.dev>
2024-12-15 21:11:23 +0800

    neovim: fix `nvim-treesitter`'s config

--- modules/home/neovim/lua/plugins/nvim-treesitter.lua
+++ modules/home/neovim/lua/plugins/nvim-treesitter.lua
@@ -1,45 +1,34 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     dependencies = {
       "nvim-treesitter/playground",
-      "JoosepAlviste/nvim-ts-context-commentstring",
       "RRethy/nvim-treesitter-endwise",
     },
-    config = function()
-      require("nvim-treesitter/configs").setup({
-        context_commentstring = {
-          enable = true,
-        },
-        endwise = {
-          enable = true,
-        },
-        highlight = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "T",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
-        },
-        playground = {
-          enable = true
-        },
-        ensure_installed = {
-          "c",
-          "javascript",
-          "lua",
-          "markdown",
-          "nix",
-          "rust",
-          "toml",
-          "vimdoc",
-        },
-      })
+    opts = {
+      endwise = {
+        enable = true,
+      },
+      highlight = {
+        enable = true,
+      },
+      playground = {
+        enable = true
+      },
+      ensure_installed = {
+        "c",
+        "javascript",
+        "lua",
+        "markdown",
+        "nix",
+        "rust",
+        "toml",
+        "vimdoc",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end
   }
 }
---
+++ modules/home/neovim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  }
+}
