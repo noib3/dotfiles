ababc94ae60539eded6bc353e3ce3b04c53e9c16
Riccardo Mazzarini <me@noib3.dev>
2024-12-16 14:28:23 +0800

    fonts: make IosevkaTerm's bold styles a bit thicker

--- fonts/monospace/iosevka-term.nix
+++ fonts/monospace/iosevka-term.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 
 {
-  name = "Iosevka Term Nerd Font";
+  name = "IosevkaTerm Nerd Font";
+  bold = "ExtraBold";
+  bold_italic = "ExtraBold Italic";
   package = pkgs.nerd-fonts.iosevka-term;
   size = _config: _program: 16.5;
 }
