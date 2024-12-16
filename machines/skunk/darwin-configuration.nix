4f5bdfbae2ba773083e1bbf82546943335aee5bd
Riccardo Mazzarini <me@noib3.dev>
2024-12-16 17:40:45 +0800

    skunk: disable `skhd` and `yabai` on macOS

--- machines/skunk/darwin-configuration.nix
+++ machines/skunk/darwin-configuration.nix
@@ -1,20 +1,8 @@
 {
-  config,
-  pkgs,
-  colorscheme,
   hostName,
   ...
 }:
 
-let
-  configDir = ../../configs;
-
-  configs = import "${configDir}" {
-    inherit (pkgs) lib;
-    inherit colorscheme config pkgs;
-    palette = import (../../palettes + "/${colorscheme}.nix");
-  };
-in
 {
   imports = [
     ../../modules/darwin/desktop.nix
@@ -23,10 +11,6 @@ in
   modules.desktop = {
     enable = true;
     inherit hostName;
-  };
-
-  services = {
-    inherit (configs) skhd yabai;
   };
 
   system.stateVersion = 5;
