9619e44348552ad0c24912cd088c89d82bece8fa
Riccardo Mazzarini <me@noib3.dev>
2024-12-19 01:18:52 +0800

    neovim: generate `palette.lua` from colorscheme's palette

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -38,5 +38,25 @@ in
     xdg.configFile.nvim = {
       source = config.lib.file.mkOutOfStoreSymlink (config.lib.mine.mkAbsolute ./.);
     };
+
+    xdg.dataFile."nvim/site/lua/generated/palette.lua" =
+      let
+        toLua =
+          value:
+          if builtins.isAttrs value then
+            value
+            |> lib.mapAttrsToList (k: v: "${k} = ${toLua v}")
+            |> builtins.concatStringsSep ", "
+            |> (str: "{ ${str} }")
+          else if builtins.isString value then
+            "'${value}'"
+          else
+            builtins.toString value;
+
+        inherit (config.modules) colorscheme;
+      in
+      {
+        text = "return ${toLua colorscheme.palette}";
+      };
   };
 }
