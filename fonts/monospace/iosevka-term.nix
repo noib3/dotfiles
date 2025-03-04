d7e6eaa85048be491713322eb0732aa9274623fb
Riccardo Mazzarini <me@noib3.dev>
2025-03-04 18:26:00 +0100

    fonts(iosevka): increase size on `skunk@darwin`

--- fonts/monospace/iosevka-term.nix
+++ fonts/monospace/iosevka-term.nix
@@ -5,5 +5,7 @@
   bold = "ExtraBold";
   bold_italic = "ExtraBold Italic";
   package = pkgs.nerd-fonts.iosevka-term;
-  size = _config: _program: 16.5;
+  size =
+    config: _program:
+    if config.machines."skunk@darwin".isCurrent then 22.75 else 16.5;
 }
