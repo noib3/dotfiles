f7ac35bc1d9a31cf1869ce1db560356a6c938a69
Riccardo Mazzarini <me@noib3.dev>
2025-03-05 18:54:01 +0100

    nix: make `nix develop` and `nix-shell` use fish

--- modules/home/nix/default.nix
+++ modules/home/nix/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   ...
@@ -6,13 +7,21 @@
 
 {
   config = {
-    home.packages =
-      with pkgs;
-      [
-        nixd
-        nixVersions.latest
-        nixfmt-rfc-style
-      ];
+    programs.fish.interactiveShellInit = lib.mkIf config.programs.fish.enable ''
+      function nix-shell --description "Start an interactive shell based on a Nix expression"
+          ${pkgs.nix-your-shell}/bin/nix-your-shell fish nix-shell -- $argv
+      end
+
+      function nix --description "Reproducible and declarative configuration management"
+          ${pkgs.nix-your-shell}/bin/nix-your-shell fish nix -- $argv
+      end
+    '';
+
+    home.packages = with pkgs; [
+      nixd
+      nixVersions.latest
+      nixfmt-rfc-style
+    ];
 
     nix = {
       package = pkgs.nixVersions.latest;
