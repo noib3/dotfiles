0ad51b709101aba82592a1877e3c36999f7e4b3b
Riccardo Mazzarini <me@noib3.dev>
2025-03-05 23:19:13 +0100

    fish: remove `bass`
    I forgot what I even needed it for

--- configs/fish/default.nix
+++ configs/fish/default.nix
@@ -51,91 +51,74 @@ in
       drs = "darwin-rebuild switch --flake ${dotfilesDir}#${config.machine.name}";
     };
 
-  interactiveShellInit =
-    ''
-      set fish_greeting ""
+  interactiveShellInit = ''
+    set fish_greeting ""
 
-      set fish_cursor_default block
-      set fish_cursor_insert line
-      set fish_cursor_replace_one underscore
-      set fish_cursor_visual block
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual block
 
-      set fish_color_user green
-      set fish_color_host white
-      set fish_color_host_remote white
-      set fish_color_cwd blue
-      set fish_color_command green
-      set fish_color_error red
-      set fish_color_quote yellow
-      set fish_color_valid_path --underline
-      set fish_color_redirection white
+    set fish_color_user green
+    set fish_color_host white
+    set fish_color_host_remote white
+    set fish_color_cwd blue
+    set fish_color_command green
+    set fish_color_error red
+    set fish_color_quote yellow
+    set fish_color_valid_path --underline
+    set fish_color_redirection white
 
-      set fish_color_param ${colors.param}
-      set fish_color_operator ${colors.operator}
-      set fish_color_autosuggestion ${colors.autosuggestion} --italics
-      set fish_color_comment ${colors.comment} --italics
-      set fish_color_end ${colors.end}
-      set fish_color_selection --background=${colors.selection_bg}
+    set fish_color_param ${colors.param}
+    set fish_color_operator ${colors.operator}
+    set fish_color_autosuggestion ${colors.autosuggestion} --italics
+    set fish_color_comment ${colors.comment} --italics
+    set fish_color_end ${colors.end}
+    set fish_color_selection --background=${colors.selection_bg}
 
-      fish_vi_key_bindings
+    fish_vi_key_bindings
 
-      bind \cA beginning-of-line
-      bind \cE end-of-line
-      bind yy fish_clipboard_copy
-      bind Y fish_clipboard_copy
-      bind p fish_clipboard_paste
+    bind \cA beginning-of-line
+    bind \cE end-of-line
+    bind yy fish_clipboard_copy
+    bind Y fish_clipboard_copy
+    bind p fish_clipboard_paste
 
-      bind -M visual \cA beginning-of-line
-      bind -M visual \cE end-of-line
+    bind -M visual \cA beginning-of-line
+    bind -M visual \cE end-of-line
 
-      bind -M insert \cA beginning-of-line
-      bind -M insert \cE end-of-line
-      bind -M insert \e\x7F backward-kill-word
-      bind -M insert \cW exit
-      bind -M insert \cG 'clear; commandline -f repaint'
-      bind -M insert \cX\cD fuzzy-cd
-      bind -M insert \cX\cE fuzzy-edit
-      bind -M insert \cX\cF fuzzy-history
-      bind -M insert \cX\cG fuzzy-kill
-      bind -M insert \cX\cR fuzzy-ripgrep-fish
-      bind -M insert \cS fuzzy-search
+    bind -M insert \cA beginning-of-line
+    bind -M insert \cE end-of-line
+    bind -M insert \e\x7F backward-kill-word
+    bind -M insert \cW exit
+    bind -M insert \cG 'clear; commandline -f repaint'
+    bind -M insert \cX\cD fuzzy-cd
+    bind -M insert \cX\cE fuzzy-edit
+    bind -M insert \cX\cF fuzzy-history
+    bind -M insert \cX\cG fuzzy-kill
+    bind -M insert \cX\cR fuzzy-ripgrep-fish
+    bind -M insert \cS fuzzy-search
 
-      # For some reason the pisces plugin needs to be sourced manually to become
-      # active.
-      source ~/.config/fish/conf.d/plugin-pisces.fish
+    # For some reason the pisces plugin needs to be sourced manually to become
+    # active.
+    source ~/.config/fish/conf.d/plugin-pisces.fish
 
-      direnv hook fish | source
+    direnv hook fish | source
 
-      ${pkgs.gnupg}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
-    ''
-    + lib.strings.optionalString isDarwin ''
-      bass source ~/.nix-profile/etc/profile.d/nix{,-daemon}.sh 2>/dev/null \
-        || true
-    '';
+    ${pkgs.gnupg}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
+  '';
 
-  plugins =
-    [
-      {
-        name = "pisces";
-        src = pkgs.fetchFromGitHub {
-          owner = "laughedelic";
-          repo = "pisces";
-          rev = "v0.7.0";
-          sha256 = "073wb83qcn0hfkywjcly64k6pf0d7z5nxxwls5sa80jdwchvd2rs";
-        };
-      }
-    ]
-    ++ lib.lists.optionals isDarwin [
-      {
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
-          sha256 = "0mb01y1d0g8ilsr5m8a71j6xmqlyhf8w4xjf00wkk8k41cz3ypky";
-        };
-      }
-    ];
+  plugins = [
+    {
+      name = "pisces";
+      src = pkgs.fetchFromGitHub {
+        owner = "laughedelic";
+        repo = "pisces";
+        rev = "v0.7.0";
+        sha256 = "073wb83qcn0hfkywjcly64k6pf0d7z5nxxwls5sa80jdwchvd2rs";
+      };
+    }
+  ];
 
   functions = {
     fuzzy-cd = builtins.readFile ./functions/fuzzy-cd.fish;
