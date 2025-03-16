b5ae7af9828af9d575fb2efd7a130de8f8a143ac
Riccardo Mazzarini <me@noib3.dev>
2025-03-16 23:00:36 +0100

    neovim: only keep errors and warnings in trouble's diagnostic popup

--- modules/home/neovim/lua/plugins/trouble.lua
+++ modules/home/neovim/lua/plugins/trouble.lua
@@ -1,26 +1,15 @@
+local home = vim.env.HOME
+
+local starts_with = function(str, start)
+  return str:sub(1, #start) == start
+end
+
 return {
   {
     "folke/trouble.nvim",
-    opts = {},
-    config = function(_, opts)
-      local trouble = require("trouble")
-
-      vim.keymap.set("n", "<C-x><C-t>", function()
-        trouble.toggle({
-          focus = true,
-          mode = "diagnostics",
-          win = {
-            type = "split",
-            size = {
-              height = 12,
-            },
-          },
-        })
-      end)
-
-      vim.keymap.set("n", "<C-x><C-x>", function()
-        trouble.toggle({
-          focus = true,
+    opts = {
+      modes = {
+        call_sites = {
           mode = "lsp_references",
           win = {
             type = "split",
@@ -28,7 +17,34 @@ return {
               height = 14,
             },
           },
-        })
+        },
+        errors_and_warnings = {
+          mode = "diagnostics",
+          filter = function(items)
+            return vim.tbl_filter(function(item)
+              return
+                  item.severity <= vim.diagnostic.severity.WARN
+                  and starts_with(item.filename, home)
+            end, items)
+          end,
+          win = {
+            type = "split",
+            size = {
+              height = 12,
+            },
+          },
+        },
+      }
+    },
+    config = function(_, opts)
+      local trouble = require("trouble")
+
+      vim.keymap.set("n", "<C-x><C-t>", function()
+        trouble.toggle({ focus = true, mode = "errors_and_warnings" })
+      end)
+
+      vim.keymap.set("n", "<C-x><C-x>", function()
+        trouble.toggle({ focus = true, mode = "call_sites" })
       end)
 
       trouble.setup(opts)
