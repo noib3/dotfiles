e8666d913364961ac1e9170b85a50af86568689a
Riccardo Mazzarini <me@noib3.dev>
2025-03-18 01:32:26 +0100

    terminfo: fix activation script

--- modules/home/terminfo/default.nix
+++ modules/home/terminfo/default.nix
@@ -42,6 +42,10 @@ in
         copyTerminfo = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           $DRY_RUN_CMD mkdir -p "${terminfo_dir}"
           $DRY_RUN_CMD cp -r "${enabledTerminal.terminfoSrc}/"* "${terminfo_dir}"
+          # The files we copied from the Nix store are read-only, so make them
+          # writable or we won't be able to update them the next time the
+          # activation script is run.
+          $DRY_RUN_CMD chmod -R +w "${terminfo_dir}"
         '';
       };
     }
