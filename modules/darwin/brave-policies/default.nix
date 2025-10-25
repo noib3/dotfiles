a2149c7852f5f1a6f79a3a6f89461ce3158bd00c
Riccardo Mazzarini <me@noib3.dev>
2025-10-25 16:46:14 +0200

    brave: more policies

--- modules/darwin/brave-policies/default.nix
+++ modules/darwin/brave-policies/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -13,15 +14,67 @@ in
     enable = mkEnableOption "Brave policies";
   };
 
-  config = mkIf cfg.enable {
-    # See https://chromeenterprise.google/policies/ and
-    # https://support.brave.app/hc/en-us/articles/360039248271-Group-Policy for
-    # the available policies.
-    system.defaults.CustomUserPreferences."com.brave.Browser" = {
-      DefaultSearchProviderEnabled = true;
-      DefaultSearchProviderName = "DuckDuckGo";
-      DefaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
-      DefaultSearchProviderSuggestURL = "https://duckduckgo.com/ac/?q={searchTerms}&type=list";
-    };
-  };
+  config = mkIf cfg.enable (
+    let
+      # See https://chromeenterprise.google/policies/ and
+      # https://support.brave.app/hc/en-us/articles/360039248271-Group-Policy
+      # for the available policies.
+      policies = {
+        BraveRewardsDisabled = true;
+        BraveWalletDisabled = true;
+        BraveAIChatEnabled = false;
+        BraveNewsDisabled = true;
+        BraveTalkDisabled = true;
+        BraveVPNDisabled = true;
+        BraveStatsPingEnabled = false;
+        BrowserSignin = 0;
+        DefaultSearchProviderEnabled = true;
+        DefaultSearchProviderName = "DuckDuckGo";
+        DefaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
+        DefaultSearchProviderSuggestURL = "https://duckduckgo.com/ac/?q={searchTerms}&type=list";
+        HomepageIsNewTabPage = true;
+        PasswordManagerEnabled = false;
+        SyncDisabled = true;
+      };
+    in
+    {
+      system.defaults.CustomUserPreferences."com.brave.Browser" = policies;
+
+      # See https://github.com/brave/brave-browser/issues/45106 for why this is
+      # needed. That issue seems to imply that this is only needed for
+      # "managed" preferences (whatever that means), but it doesn't seem to
+      # hurt to add them all.
+      system.activationScripts.extraActivation.text =
+        let
+          managedPrefsPlistFile =
+            pkgs.runCommand "brave-policies.plist"
+              {
+                nativeBuildInputs = [ pkgs.python3 ];
+              }
+              ''
+                python3 << EOF
+                import plistlib
+                import json
+                policies = json.loads('${builtins.toJSON policies}')
+                with open('$out', 'wb') as f:
+                    plistlib.dump(policies, f)
+                EOF
+              '';
+          managedPrefsDirPath = "/Library/Managed Preferences";
+          managedPrefsPlistPath = "${managedPrefsDirPath}/com.brave.Browser.plist";
+        in
+        ''
+          mkdir -p "${managedPrefsDirPath}"
+          chown root:wheel "${managedPrefsDirPath}"
+          chmod 755 "${managedPrefsDirPath}"
+
+          cp -f "${managedPrefsPlistFile}" "${managedPrefsPlistPath}"
+          chown root:wheel "${managedPrefsPlistPath}"
+          chmod 644 "${managedPrefsPlistPath}"
+
+          # See https://github.com/brave/brave-browser/issues/45106#issuecomment-3089894139
+          killall cfprefsd || true
+        '';
+    }
+  );
 }
