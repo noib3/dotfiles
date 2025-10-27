07872f64b207f84409a8d62a56d772e3a4171af2
Riccardo Mazzarini <me@noib3.dev>
2025-10-27 02:17:18 +0100

    fonts(iosevka): make size a bit smaller on stolen-bride

--- fonts/monospace/iosevka-term.nix
+++ fonts/monospace/iosevka-term.nix
@@ -7,5 +7,10 @@
   package = pkgs.nerd-fonts.iosevka-term;
   size =
     config: _program:
-    if config.machines."skunk@darwin".isCurrent then 22.75 else 16.5;
+    if config.machines."skunk@darwin".isCurrent then
+      22.75
+    else if config.machines.stolen-bride.isCurrent then
+      22.0
+    else
+      16.5;
 }
