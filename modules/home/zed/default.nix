4dd6e400a6ba44f12a5188b4313b1df8673be911
Riccardo Mazzarini <me@noib3.dev>
2025-11-16 19:39:38 +0100

    zed: configure key bindings

--- modules/home/zed/default.nix
+++ modules/home/zed/default.nix
@@ -17,11 +17,57 @@ in
   config = mkIf cfg.enable {
     programs.zed-editor = {
       enable = true;
+      mutableUserKeymaps = false;
       mutableUserSettings = false;
       themes = import ./themes;
-      userKeymaps = {
+      userKeymaps = [
+        {
+          context = "Editor";
+          bindings = {
+            # Scroll up/down by half a page.
+            "cmd-up" = "vim::ScrollUp";
+            "cmd-down" = "vim::ScrollDown";
 
-      };
+            # Scroll up/down visual lines (instead of buffer lines).
+            "up" = "editor::MoveUp";
+            "down" = "editor::MoveDown";
+          };
+        }
+        {
+          context = "Editor && vim_mode == normal";
+          bindings = {
+            # Create splits.
+            "s up" = "pane::SplitUp";
+            "s down" = "pane::SplitDown";
+            "s left" = "pane::SplitLeft";
+            "s right" = "pane::SplitRight";
+
+            # Navigate splits.
+            "shift-up" = [
+              "workspace::ActivatePaneInDirection"
+              "Up"
+            ];
+            "shift-down" = [
+              "workspace::ActivatePaneInDirection"
+              "Down"
+            ];
+            "shift-left" = [
+              "workspace::ActivatePaneInDirection"
+              "Left"
+            ];
+            "shift-right" = [
+              "workspace::ActivatePaneInDirection"
+              "Right"
+            ];
+          };
+        }
+        {
+          context = "vim_mode == visual";
+          bindings = {
+            shift-s = "vim::PushAddSurrounds";
+          };
+        }
+      ];
       userSettings =
         let
           buffer_font = config.fonts.monospace;
