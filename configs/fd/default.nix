5d4fc10dab58e5cdb9e6f26e13c26cc856ab1302
Riccardo Mazzarini <me@noib3.dev>
2026-01-21 02:52:42 +0100

    fd: ignore `**/.config` and `/Applications` on macOS

--- configs/fd/default.nix
+++ configs/fd/default.nix
@@ -3,36 +3,37 @@
 {
   enable = true;
 
-  ignores =
-    [
-      "**/.cache"
-      "**/.cargo"
-      "**/.direnv"
-      "**/.dropbox"
-      "**/.dropbox.cache"
-      "**/.git"
-      "**/.ipython"
-      "**/.local"
-      "**/.stfolder"
-      "**/gnupg/*"
-      "**/target"
-      "*.aux"
-      "*.bbl"
-      "*.bcf"
-      "*.blg"
-      "*.dropbox"
-      "*.loe"
-      "*.log"
-      "*.out"
-      "*.run.xml"
-      "*.synctex(busy)"
-      "*.synctex.gz"
-      "*.toc"
-    ]
-    ++ lib.lists.optionals pkgs.stdenv.isDarwin [
-      "/.Trash"
-      "/Library"
-      "/Music"
-      "/Pictures"
-    ];
+  ignores = [
+    "**/.cache"
+    "**/.cargo"
+    "**/.config"
+    "**/.direnv"
+    "**/.dropbox"
+    "**/.dropbox.cache"
+    "**/.git"
+    "**/.ipython"
+    "**/.local"
+    "**/.stfolder"
+    "**/gnupg/*"
+    "**/target"
+    "*.aux"
+    "*.bbl"
+    "*.bcf"
+    "*.blg"
+    "*.dropbox"
+    "*.loe"
+    "*.log"
+    "*.out"
+    "*.run.xml"
+    "*.synctex(busy)"
+    "*.synctex.gz"
+    "*.toc"
+  ]
+  ++ lib.lists.optionals pkgs.stdenv.isDarwin [
+    "/.Trash"
+    "/Applications"
+    "/Library"
+    "/Music"
+    "/Pictures"
+  ];
 }
