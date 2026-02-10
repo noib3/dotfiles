5073d606a25a40585d0bcba521afab854379d48b
Riccardo Mazzarini <me@noib3.dev>
2026-02-10 21:54:15 +0100

    opencode: disable blinking and set line cursor

--- modules/home/opencode/default.nix
+++ modules/home/opencode/default.nix
@@ -9,6 +9,19 @@
 with lib;
 let
   cfg = config.modules.opencode;
+
+  opencodePackage =
+    (inputs.opencode.packages.${pkgs.stdenv.system}.default).overrideAttrs
+      (old: {
+        # Add patch from https://github.com/anomalyco/opencode/pull/11300 to be
+        # able to set the cursor style and disable blinking.
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/anomalyco/opencode/commit/062837c4caf574846c8b4f2b7a43fe2bb21531e8.patch";
+            hash = "sha256-oSFUS7PONbcLh/JZHDgcHJhtPRq3Tbl7hE7F/vN6UpI=";
+          })
+        ];
+      });
 in
 {
   options.modules.opencode = {
@@ -18,9 +31,13 @@ in
   config = mkIf cfg.enable {
     programs.opencode = {
       enable = true;
-      package = inputs.opencode.packages.${pkgs.stdenv.system}.default;
+      package = opencodePackage;
       settings = {
         theme = "system";
+        tui = {
+          cursor_blink = false;
+          cursor_style = "line";
+        };
       };
     };
   };
