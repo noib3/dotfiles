36d81749f57a542f6533e14f628e279cd27f4b1d
Riccardo Mazzarini <me@noib3.dev>
2026-02-27 14:33:47 +0100

    rust: fix dynamic loading issue on macOS
    See https://github.com/oxalica/rust-overlay/issues/248

--- modules/home/rust/default.nix
+++ modules/home/rust/default.nix
@@ -9,6 +9,38 @@
 with lib;
 let
   cfg = config.modules.rust;
+
+  nightlyToolchain = pkgs.rust-bin.selectLatestNightlyWith (
+    toolchain:
+    toolchain.minimal.override {
+      extensions = [
+        "clippy"
+        "miri"
+        # Needed by `cargo-llvm-cov`.
+        "llvm-tools"
+        "rust-analyzer"
+        # Needed by `rust-analyzer` to index `std`.
+        "rust-src"
+        "rustfmt"
+      ];
+    }
+  );
+
+  # Wrap all the executables in the toolchain to get around
+  # https://github.com/oxalica/rust-overlay/issues/248
+  nightlyToolchainWrapped =
+    pkgs.runCommand "rust-nightly-toolchain-wrapped"
+      {
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      }
+      ''
+        mkdir -p "$out/bin"
+
+        for exe in "${nightlyToolchain}"/bin/*; do
+          makeWrapper "$exe" "$out/bin/$(basename "$exe")" \
+            --prefix DYLD_FALLBACK_LIBRARY_PATH : "${nightlyToolchain}/lib"
+        done
+      '';
 in
 {
   options.modules.rust = {
@@ -19,26 +51,15 @@ in
     home.packages =
       with pkgs;
       [
-        (rust-bin.selectLatestNightlyWith (
-          toolchain:
-          toolchain.minimal.override {
-            extensions = [
-              "clippy"
-              "miri"
-              # Needed by `cargo-llvm-cov`.
-              "llvm-tools"
-              "rust-analyzer"
-              # Needed by `rust-analyzer` to index `std`.
-              "rust-src"
-              "rustfmt"
-            ];
-          }
-        ))
+        nightlyToolchain
         cargo-criterion
         cargo-deny
         cargo-expand
         cargo-flamegraph
         cargo-fuzz
+      ]
+      ++ lib.lists.optionals pkgs.stdenv.isDarwin [
+        (lib.hiPrio nightlyToolchainWrapped)
       ]
       # cargo-llvm-cov is currently broken on macOS.
       ++ lib.lists.optionals (!pkgs.stdenv.isDarwin) [ cargo-llvm-cov ];
