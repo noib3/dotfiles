ef3d4ebdbed4fb93a95aadbaf91fddb02ceb585a
Riccardo Mazzarini <me@noib3.dev>
2026-04-30 01:34:00 +0200

    neovim(conform): enable `uncrustify`

--- modules/home/neovim/config/lua/plugins/conform.lua
+++ modules/home/neovim/config/lua/plugins/conform.lua
@@ -1,8 +1,20 @@
 ---@diagnostic disable-next-line: undefined-field
 require("conform").setup({
   formatters_by_ft = {
+    c = { "uncrustify" },
     lua = { "stylua" },
     sh = { "shfmt" },
+  },
+  formatters = {
+    -- Only enable uncrustify if the project has an uncrustify.cfg.
+    uncrustify = {
+      condition = function(_, ctx)
+        return vim.fs.find(
+          "uncrustify.cfg",
+          { upward = true, path = ctx.dirname }
+        )[1] ~= nil
+      end,
+    },
   },
   notify_no_formatters = false,
 })
