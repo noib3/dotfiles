7e9bae0bae9e6aea55d33e5f9ea96b5cd2d030f2
Riccardo Mazzarini <me@noib3.dev>
2026-05-30 18:57:45 +0200

    ghostty: remove split-related keybindings

--- modules/home/ghostty/keybinds.nix
+++ modules/home/ghostty/keybinds.nix
@@ -2,16 +2,6 @@
 
 [
   "alt+escape=reload_config"
-  # Create splits.
-  "super+g>left=new_split:left"
-  "super+g>right=new_split:right"
-  "super+g>up=new_split:up"
-  "super+g>down=new_split:down"
-  # Navigate splits (only if one exists in the given direction).
-  "performable:shift+left=goto_split:left"
-  "performable:shift+right=goto_split:right"
-  "performable:shift+up=goto_split:up"
-  "performable:shift+down=goto_split:down"
   # Unbind these default keybindings.
   "super+d=unbind"
   "super+k=unbind"
