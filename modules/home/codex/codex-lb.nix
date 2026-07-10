921ce1cac1ae5dbb869e4226c11f59035f68c57f
Riccardo Mazzarini <me@noib3.dev>
2026-07-10 13:53:59 +0200

    codex: use `codex-lb` from main and patch it to support account fallback if model fails

--- modules/home/codex/codex-lb.nix
+++ modules/home/codex/codex-lb.nix
@@ -8,12 +8,64 @@
 
 let
   pname = "codex-lb";
-  version = "1.20.0-beta.1";
+  version = "1.20.2-beta.1-main-20260710";
 
-  src = fetchzip {
-    url = "https://github.com/Soju06/codex-lb/releases/download/v${version}/codex_lb-1.20.0b1.tar.gz";
-    hash = "sha256-KuENk/M09euJNo+pq8sQ0Ghuk1Sqr+mXK6yF34TG9GY=";
+  bun2nix = inputs.bun2nix.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+  mainSrc = pkgs.applyPatches {
+    name = "${pname}-${version}-patched-source";
+    src = fetchzip {
+      url = "https://github.com/Soju06/codex-lb/archive/b0f5ea8b03a6b7685d281d74350a22377e05d72e.tar.gz";
+      hash = "sha256-h8TnZUUys+AOTX/69EysccZMdXXP042ofe9U5xOUJGA=";
+    };
+    patches = [
+      ./patches/account-model-failover.patch
+      ./patches/model-catalog-union.patch
+    ];
   };
+
+  frontendBunNix =
+    pkgs.runCommand "${pname}-frontend-bun.nix" { nativeBuildInputs = [ bun2nix ]; }
+      ''
+        bun2nix \
+          --lock-file ${mainSrc}/frontend/bun.lock \
+          --output-file $out
+      '';
+
+  frontend = pkgs.stdenvNoCC.mkDerivation {
+    pname = "${pname}-frontend";
+    inherit version;
+    src = mainSrc;
+    sourceRoot = "${mainSrc.name}/frontend";
+    postUnpack = ''
+      chmod -R u+w ${mainSrc.name}
+    '';
+
+    nativeBuildInputs = [ bun2nix.hook ];
+    bunDeps = bun2nix.fetchBunDeps {
+      bunNix = frontendBunNix;
+    };
+    dontRunLifecycleScripts = true;
+
+    buildPhase = ''
+      runHook preBuild
+      bun run build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      cp -R ../app/static $out
+      runHook postInstall
+    '';
+  };
+
+  src = pkgs.runCommand "${pname}-${version}-source" { } ''
+    mkdir -p "$out"
+    cp -R ${mainSrc}/. "$out"
+    chmod -R u+w "$out"
+    cp -R ${frontend} "$out/app/static"
+  '';
 
   workspace = inputs.uv2nix.lib.workspace.loadWorkspace {
     workspaceRoot = src;
